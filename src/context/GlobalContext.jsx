f0f010e8f1d11b80d5d6ebf3730259f1dc3f4cdf
giorgiadifranco <giorgiadf08@gmail.com>
2024-11-28 12:50:19 +0100

    fix function provider

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,5 +1,5 @@
-import { createContext } from 'react';
-import { useState }
+import { createContext, useState, useEffect } from 'react';
+
 
 //creo il contesto
 
@@ -10,13 +10,39 @@ const FilmsContext = createContext();
 
 export default FilmsContextProvider ({children}){
 
-    const [ films, setFilms] = useState ({})
-    const url_films = `https://api.themoviedb.org/3/search/movie?api_key=3d5a0c215d6ca4a4fe980f405ca9b287&query=${title}`
+    const [ films, setFilms] = useState ([])
+    
 
-
-    function fetchResults(url_films){
-
+    function fetchResults(title){
         
+        const url_films = `https://api.themoviedb.org/3/search/movie?api_key=3d5a0c215d6ca4a4fe980f405ca9b287&query=${title}`;
+
+
+        fetch(url_films)
+        .then(resp =>resp.json())
+        .then(({results}) =>{
+            console.log(results);
+            setPosts(results)
+            
+        })
+
+        useEffect(fetchResults, [])
+        
+        {/* fornire il contesto ai children*/}
+        return(
+
+            
+            <FilmsContext.Provider value={{films}}>
+
+                {children}
+
+            </FilmsContext.Provider>
+
+
+
+        )
+
+
 
     }
 
