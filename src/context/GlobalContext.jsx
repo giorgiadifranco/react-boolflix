82622e88a165452fe926d4c31caba153afc75b03
giorgiadifranco <giorgiadf08@gmail.com>
2024-11-28 12:05:11 +0100

    fix function provider

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,14 +1,23 @@
-import { createContext } from 'react'
+import { createContext } from 'react';
+import { useState }
 
 //creo il contesto
 
 
 const FilmsContext = createContext(); 
 
-//creare provider
+//creare provider --> cosa fa? servire i dati tramite chiamata fetch
 
 export default FilmsContextProvider ({children}){
 
-    
+    const [ films, setFilms] = useState ({})
+    const url_films = `https://api.themoviedb.org/3/search/movie?api_key=3d5a0c215d6ca4a4fe980f405ca9b287&query=${title}`
+
+
+    function fetchResults(url_films){
+
+        
+
+    }
 
 }
