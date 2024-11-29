cd8f52eac351f1302f142ee154faae98b1039df8
giorgiadifranco <giorgiadf08@gmail.com>
2024-11-29 15:58:42 +0100

    fix return

--- src/components/FilmList.jsx
+++ src/components/FilmList.jsx
@@ -2,7 +2,7 @@ import { useFilmsContext } from '../context/GlobalContext';
 import HandleFlagLanguage from './FlagLanguage';
 
 function FilmsList() {
-    const { films } = useFilmsContext();
+    const { films, HandleImageMovies } = useFilmsContext();
 
     
 
@@ -12,6 +12,7 @@ function FilmsList() {
                 {films && films.map((film, index) => (
                     <li key={index}>
                         <h3>{film.title}</h3>
+                        <img src={HandleImageMovies} alt={film.title} />
                         <p><strong>Titolo originale:</strong> {film.original_title}</p>
                         <p><strong>Lingua:</strong> <HandleFlagLanguage language={film.original_language} /></p>
                         <p><strong>Voto:</strong> {film.vote_average}</p>
